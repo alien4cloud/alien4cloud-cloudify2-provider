73cfa010df2f27ab99746303180e4e24abfb5034
Igor Ngouagna <igor.ngouagna@fastconnect.fr>
2015-05-06 16:01:47 +0200

    fixed broken IT tests

--- src/test/resources/csars/test-types-1.0-SNAPSHOT/scripts/testCompute_stop.groovy
+++ src/test/resources/csars/test-types-1.0-SNAPSHOT/scripts/testCompute_stop.groovy
@@ -1,4 +1,4 @@
 import org.apache.commons.validator.routines.InetAddressValidator;
-
+def ipValidator = InetAddressValidator.getInstance();
 assert MY_IP && ipValidator.isValidInet4Address(MY_IP): "Empty or not valid env var MY_IP"
 println "MY_IP : ${MY_IP}"
