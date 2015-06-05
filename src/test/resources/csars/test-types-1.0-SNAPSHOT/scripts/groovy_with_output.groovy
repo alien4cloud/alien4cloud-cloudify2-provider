f81834878b6beb2948c6b11d030da0045e5aeeaf
Igor Ngouagna <igor.ngouagna@fastconnect.fr>
2015-06-05 11:35:22 +0200

    test improvement for get_operation_output

--- src/test/resources/csars/test-types-1.0-SNAPSHOT/scripts/groovy_with_output.groovy
+++ src/test/resources/csars/test-types-1.0-SNAPSHOT/scripts/groovy_with_output.groovy
@@ -5,5 +5,9 @@ def randomOutPut = 1234567890
 println "Setting output_from_create output...<${randomOutPut}>"
 output_from_create = randomOutPut
 
+def testForConcat = "thisIsATestForConcat"
+logger.log("Setting output test_for_concat output...<${testForConcat}> ")
+test_for_concat = testForConcat
+
 return "done"
 
