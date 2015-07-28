f14964cea8b03471b4af54ef59b9f193eabceb0b
vuminhkh <minh.khang.vu.mkv@gmail.com>
2015-07-28 17:10:38 +0200

    Rest client bug

--- src/main/java/alien4cloud/paas/cloudify2/rest/RestExecutor.java
+++ src/main/java/alien4cloud/paas/cloudify2/rest/RestExecutor.java
@@ -3,9 +3,6 @@ package alien4cloud.paas.cloudify2.rest;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,12 +16,8 @@ import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.PoolingClientConnectionManager;
 import org.apache.http.util.EntityUtils;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.Scope;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
 
 import alien4cloud.paas.cloudify2.exception.PaaSEventException;
 import alien4cloud.rest.utils.JsonUtil;
@@ -32,28 +25,9 @@ import alien4cloud.rest.utils.JsonUtil;
 @AllArgsConstructor
 @Setter
 @Slf4j
-@Component("cloudify-rest-executor-bean")
-@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class RestExecutor {
 
     private DefaultHttpClient httpClient;
-
-    public RestExecutor() {
-        PoolingClientConnectionManager cm = new PoolingClientConnectionManager();
-        cm.setMaxTotal(200);
-        cm.setDefaultMaxPerRoute(20);
-        this.httpClient = new DefaultHttpClient(cm);
-    }
-
-    @PostConstruct
-    public void postConstruct() {
-        log.info("Instantiated rest executor " + this);
-    }
-
-    @PreDestroy
-    public void shutDown() {
-        this.httpClient.getConnectionManager().shutdown();
-    }
 
     public String doGet(URIBuilder builder, boolean failOnError) throws URISyntaxException, IOException {
         Response response = doGet(builder);
@@ -117,8 +91,12 @@ public class RestExecutor {
         }
     }
 
+    public void shutdown() {
+        this.httpClient.getConnectionManager().shutdown();
+    }
+
     @Getter
-    private class Response {
+    private static class Response {
         private int status;
         private String errorReason;
         private String response;
